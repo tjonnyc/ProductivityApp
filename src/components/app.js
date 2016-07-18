4d7a6701bdee540889c92ee2a17623ebd38abf71
Tyler Hannasch <Tyler Hannasch>
2016-07-18 17:54:21 -0400

    Now opens in a new tab

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+    	<div>
+      		<div>React simple starter</div>
+      		<button>Create Tab</button>
+  		</div>
     );
   }
 }
