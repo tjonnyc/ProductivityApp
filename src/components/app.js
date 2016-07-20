e0c04b8a96cb1c02373cb0c2fe8b12cda9689a67
Ryan Newton <newton1988@gmail.com>
2016-07-19 23:27:45 -0500

    Makes all indents tabs

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  render() {
-    return (
-    	<div>
-      		<div>React simple starter</div>
-      		<button>Create Tab</button>
-  		</div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<div>React simple starter</div>
+				<button>Create Tab</button>
+			</div>
+		);
+	}
 }
