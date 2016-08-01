aa5a492ae9cf2ebc94c6fe7ca9c08a603495ea50
Tyler Hannasch <Tyler Hannasch>
2016-08-02 07:12:21 +0800

    Added monitor, sync, and in process of adding display

--- background.js
+++ background.js
@@ -1,10 +1,40 @@
-console.log("background.js Started");
+console.log("Background.js Started!");
+console.log("Chrome Object: ", chrome);
 
+let timeSegments = [];
+
+//Opens a new tab and shows index.html
 function openBehaviorViewer(e)	{
-	console.log("Fired");
 	chrome.tabs.create({
 		url: "./index.html"
 	});
 }
 
+function recordTimeSegment(url) {
+	timeSegments.push({
+		url,
+		dateTime: new Date()
+	});
+
+	console.log("Time Segments: ", timeSegments);
+
+	chrome.storage.sync.set({timeSegments});
+
+	/*chrome.storage.sync.get("timeSegments", (items) => {
+		console.log(items);
+	});*/
+}
+
+
+function tabUpdated(tabID, changeInfo, tab) {
+	recordTimeSegment(tab.url);
+}
+
+function activeTabChanged(activeInfo) {
+	chrome.tabs.query({ active: true}, (tabs) => recordTimeSegment(tabs[0].url))
+}
+
+//Add our event listeners
+chrome.tabs.onActivated.addListener(activeTabChanged);
+chrome.tabs.onUpdated.addListener(tabUpdated);
 chrome.browserAction.onClicked.addListener(openBehaviorViewer);
--- src/components/app.js
+++ src/components/app.js
@@ -3,34 +3,66 @@ import Chart from 'chart.js';
 
 export default class App extends Component {
   	
-  	render() {
+	constructor(props) {
+		super(props);		
+	}
 
-  		function createChart(referencedChart)	{
-  			var myChart = new Chart(referencedChart, {
-			    type: 'pie',
-			    data: {
-			        labels: ["Facebook", "Netflix", "Gmail", "Google Finance", "Tech Crunch", "Other"],
-			        datasets: [{
-			            label: 'Avg # of Minutes per Day',
-			            data: [35, 61, 10, 5, 5, 25],
-			            backgroundColor: [
-			                'rgba(255, 99, 132, 0.2)',
-			                'rgba(54, 162, 235, 0.2)',
-			                'rgba(255, 206, 86, 0.2)',
-			                'rgba(75, 192, 192, 0.2)',
-			                'rgba(153, 102, 255, 0.2)',
-			                'rgba(255, 159, 64, 0.2)'
-			            ]
-			        }]
-			    },
-			    options: {
-			    	
-			    }
+	render() {
+
+		function createChart(referencedChart)	{
+			
+			let timeSegments = [];
+
+			//Retrieves Time Segments Object from Sync
+			//console.log("chrome.storage.sync: ", chrome.storage.sync);
+
+				chrome.storage.sync.get("timeSegments", (items) => {
+					console.log("Time segments array", items);
+				//timeSegments = items["timeSegments"];
+				})
+
+			console.log("Time Segments Object: ", timeSegments);
+
+
+			let websites = timeSegments.reduce(function(prev, curr, index, array) {
+				
+				let timeElapsed = 0;
+				index === 0 ? timeElapsed = 0 : timeElapsed = (curr.dateTime - prev.dateTime);
+
+				if(Object.keys(prev).indexOf(curr.url) === -1) {
+					return Object.assign(prev, {url: curr.url, timeElapsed});
+				} else {
+					return Object.assign(prev, {url: curr.url, timeElapsed: prev[curr.url] + timeElapsed});
+				}
+			}, {});
+
+			console.log("Synthesized Array of Websites: ", websites);
+
+			var myChart = new Chart(referencedChart, {
+		    type: 'pie',
+		    data: {
+		        labels: ["Facebook", "Netflix", "Gmail", "Google Finance", "Tech Crunch", "Other"],
+		        datasets: [{
+		            label: 'Avg # of Minutes per Day',
+		            data: [35, 61, 10, 5, 5, 25],
+		            backgroundColor: [
+		                'rgba(255, 99, 132, 0.2)',
+		                'rgba(54, 162, 235, 0.2)',
+		                'rgba(255, 206, 86, 0.2)',
+		                'rgba(75, 192, 192, 0.2)',
+		                'rgba(153, 102, 255, 0.2)',
+		                'rgba(255, 159, 64, 0.2)'
+		            ]
+		        }]
+		    },
+		    options: {
+		    	
+		    }
 			});
-  		}
+		}
 
-    	return (
-    		<canvas id="myChart" width="400" height="400" ref={createChart} />
-    	);
-  	}
+  	return (
+  		<canvas id="myChart" width="400" height="400" ref={createChart} />
+  	);
+	}
 }
