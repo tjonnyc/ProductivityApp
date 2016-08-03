51f7fd01db52de9cf9552561b9f2eff9f479db5c
Ryan Newton <newton1988@gmail.com>
2016-08-03 16:15:44 +0800

    Cleans up app.js formatting

--- src/components/app.js
+++ src/components/app.js
@@ -13,18 +13,18 @@ export default class App extends Component {
 
 	createChart(referencedChart) {
 		//Retrieves Time Segments Object from Sync			
-		chrome.storage.sync.get("timeSegments",(items) => {				
+		chrome.storage.sync.get("timeSegments",(items) => {
 			let websites = this.consolidateTimeSegments(items["timeSegments"]);
 			console.log("Consolidated Websites: ", websites);
 			
 			this.drawChart(referencedChart, websites);
 		});
-	}		
+	}
 
 	consolidateTimeSegments(timeSegments) {
 		console.log("Time Segments Object: ", timeSegments);
 
-		return timeSegments.reduce(function(prev, curr, index, array) {				
+		return timeSegments.reduce(function(prev, curr, index, array) {
 			let timeElapsed = 0;
 			index === 0 ? timeElapsed = 0 : timeElapsed = (curr.dateTime - array[index-1].dateTime);
 
@@ -40,36 +40,35 @@ export default class App extends Component {
 		}, []);
 	}
 
-	drawChart(referencedChart, websites)	{
+	drawChart(referencedChart, websites) {
 
 		console.log("Synthesized Array of Websites: ", websites);
 
 		var myChart = new Chart(referencedChart, {
-	    type: 'pie',
-	    data: {
-	        labels: websites.map((item) => {return item.url}),
-	        datasets: [{
-	            label: 'Avg # of Minutes per Day',
-	            data: websites.map((item) => {return item.timeElapsed}),
-	            backgroundColor: [
-	                'rgba(255, 99, 132, 0.2)',
-	                'rgba(54, 162, 235, 0.2)',
-	                'rgba(255, 206, 86, 0.2)',
-	                'rgba(75, 192, 192, 0.2)',
-	                'rgba(153, 102, 255, 0.2)',
-	                'rgba(255, 159, 64, 0.2)'
-	            ]
-	        }]
-	    },
-	    options: {
-	    	
-	    }
+			type: 'pie',
+			data: {
+				labels: websites.map((item) => {return item.url}),
+				datasets: [{
+					label: 'Avg # of Minutes per Day',
+					data: websites.map((item) => {return item.timeElapsed}),
+					backgroundColor: [
+						'rgba(255, 99, 132, 0.2)',
+						'rgba(54, 162, 235, 0.2)',
+						'rgba(255, 206, 86, 0.2)',
+						'rgba(75, 192, 192, 0.2)',
+						'rgba(153, 102, 255, 0.2)',
+						'rgba(255, 159, 64, 0.2)'
+					]
+				}]
+			},
+			options: {
+			}
 		});
-	}	
+	}
 
 	render() {
-  	return (
-  		<canvas id="myChart" width="400" height="400" ref={this.createChart} />
-  	);
+		return (
+			<canvas id="myChart" width="400" height="400" ref={this.createChart} />
+		);
 	}
 }
