db5bb9b12df1e9ce9c3623359ec90b590df3faef
Tyler Hannasch <tyler.hannasch@gmail.com>
2016-10-17 20:38:51 -0700

    Added CORS handling to test

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'chrome-extension://baimoafajbhdoaeaaaemjnlhaekhefdg/');
 
     // Request methods you wish to allow
-    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
