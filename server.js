174ca376652732cbc0274fd425fd3f9cc439f2e9
Tyler Hannasch <tyler.hannasch@gmail.com>
2016-10-17 21:14:11 -0700

    Added CORS handling to test

--- server.js
+++ server.js
@@ -15,10 +15,16 @@ connection.connect();
 app.use(express.static('public'));
 
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  //res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-  //res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  next();
+		console.log(req.method);
+    if ('OPTIONS' == req.method) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+      res.send(200);
+    }
+    else {
+      next();
+    }
 };
 
 app.use(allowCrossDomain);
