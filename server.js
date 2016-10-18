0e66b4d6e5100e64ab9e9dda811fcb057c94b6f6
Tyler Hannasch <tyler.hannasch@gmail.com>
2016-10-17 20:57:36 -0700

    Added CORS handling to test

--- server.js
+++ server.js
@@ -14,25 +14,20 @@ connection.connect();
 
 app.use(express.static('public'));
 
-// Add headers
-app.use(function (req, res, next) {
-		console.log("header middleware called");
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+var allowCrossDomain = function(req, res, next) {
+		console.log(req.method);
+    if ('OPTIONS' == req.method) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+app.use(allowCrossDomain);
 
 function encrypt(value, key) {
 	return "AES_ENCRYPT('" + value + "', UNHEX(SHA2('" + key + "',512)))";
