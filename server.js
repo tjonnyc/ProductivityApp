05dd5c031e31e4f6f0063752f4d651a0a51be557
Tyler Hannasch <tyler.hannasch@gmail.com>
2016-10-17 20:43:14 -0700

    Added CORS handling to test

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.static('public'));
 
 // Add headers
 app.use(function (req, res, next) {
-
+		console.log("header middleware called");
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'chrome-extension://baimoafajbhdoaeaaaemjnlhaekhefdg/');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -28,7 +28,7 @@ app.use(function (req, res, next) {
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    //res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
     next();
